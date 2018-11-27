3a42e6d0f31b8755085a8a3f600ae6b4d647cf47
b-akram <ga96me@gmail.com>
2018-11-27 21:35:44 +0100

    added createReserve  functionnality

--- src/com/telly/controllers/BusController.java
+++ src/com/telly/controllers/BusController.java
@@ -12,16 +12,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.telly.dao.Bus;
+import com.telly.dao.FormValidationGroup;
+import com.telly.service.BusService;
 
 @Controller
 public class BusController {
  
-
+	@Autowired
+	BusService busService; 
+	
 	@RequestMapping("/createtrip")
 	public String reserveBus(Model model, Principal principal) {
 
 		model.addAttribute("bus", new Bus());
 
 		return "createtrip";
+	}
+
+	@RequestMapping(value = "/createreserve", method = RequestMethod.POST)
+	public String createReserve(@Validated(FormValidationGroup.class) Bus bus, BindingResult result,
+			Principal principal) {
+
+		if (result.hasErrors()) {
+			return "reservebus";
+		}
+
+		busService.create(bus);
+
+		return "home";
+
 	}
 }
