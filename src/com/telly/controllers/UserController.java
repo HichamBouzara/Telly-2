1ece5f0544e98941747bac1aa3ab9209f61c13b3
b-akram <ga96me@gmail.com>
2018-11-27 21:28:29 +0100

    added getReserveBook  functionnality

--- src/com/telly/controllers/UserController.java
+++ src/com/telly/controllers/UserController.java
@@ -44,7 +44,21 @@ public class UserController {
 
 	}
 	 
+	@RequestMapping(value = "/getreservations", method = RequestMethod.GET)
+	public String getReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, Model model, Principal principal) {
+		
+		
+		String username = principal.getName();
+		reserve.getUser().setUsername(username);
+		
+		List<Reserve> reserves = reserveService.getReserves(username);
+		model.addAttribute("reserves", reserves);
+		System.out.println(reserves);
 	
+		
+		return "home";
+
+	}
 
 }
 
