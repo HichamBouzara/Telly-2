2b991a6e35cef9446dd200bccd299227871cb9c2
Hichem Bouzara <fa_bouzara@esi.dz>
2018-11-27 21:28:55 +0100

    Added createUser functionality

--- src/com/telly/controllers/UserController.java
+++ src/com/telly/controllers/UserController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.telly.dao.User;
+import com.telly.dao.FormValidationGroup;
+import com.telly.service.UserService;
 
 @Controller
 public class UserController {
@@ -32,7 +34,21 @@ public class UserController {
 		
 		return "createaccount";
 	}
+	@RequestMapping(value = "/createuser", method = RequestMethod.POST)
+	public String createUser(@Validated(FormValidationGroup.class) User user, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return "createaccount";
+		}
+		
+		user.setAuthority("ROLE_USER");
+		user.setEnabled(true);
 
+		userService.create(user);
+		
+		return "home";
+
+	}
 
 }
 
