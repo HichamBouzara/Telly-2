94b22ff261b2189dacf03ecfdaacd4065b04e512
Hichem Bouzara <fa_bouzara@esi.dz>
2018-11-27 21:18:45 +0100

    Added showLogin functionality

---
+++ src/com/telly/controllers/UserController.java
@@ -0,0 +1,27 @@
+package com.telly.controllers;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
+public class UserController {
+
+	@RequestMapping("/login")
+	public String showLogin() {
+		return "login";
+	}
+
+}
+
+
+
